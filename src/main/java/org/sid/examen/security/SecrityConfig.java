1f618434d4b064b1471ebd1db57503baa1c4302a
rabihrajaa <rajaarabihh@gmail.com>
2025-05-18 21:44:26 +0100

    first commit

---
+++ src/main/java/org/sid/examen/security/SecrityConfig.java
@@ -0,0 +1,4 @@
+package org.sid.examen.security;
+
+public class SecrityConfig {
+}
---
+++ src/test/java/org/sid/examen/ExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
